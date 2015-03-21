be775f0e48df4c8bcb0d78aa5229086b4ba4eb3b
Abraham Arce <xe1gyq@gmail.com>
2015-03-21 07:31:30 -0700

    Android: Android Internals Product Binary
    Include package compilation for our binary and reorder the variables
    under Android.mk
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/netavam/balam/hellobalam/Android.mk
+++ android/netavam/balam/hellobalam/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := eng
 LOCAL_SRC_FILES = hellobalam.c
-LOCAL_MODULE = HelloBalam
-LOCAL_MODULE_TAGS = eng
+LOCAL_CFLAGS += -g -O0
+LOCAL_MODULE := hellobalam
 
 include $(BUILD_EXECUTABLE)
