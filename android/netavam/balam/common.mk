d0ce5d9bebc7c8ba93d3cd97fc30a692b06607c2
Abraham Arce <xe1gyq@gmail.com>
2015-03-21 14:34:36 -0700

    Android: Product Kernel Driver
    For information purposes this balamator driver, it will register as
    /sys/kernel/balamator/status
    
    Balam by Netavam Inc.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/netavam/balam/common.mk
+++ android/netavam/balam/common.mk
@@ -3,3 +3,6 @@ include $(call all-subdir-makefiles)
 
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
+# Enable our custom kernel
+# LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+# PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
