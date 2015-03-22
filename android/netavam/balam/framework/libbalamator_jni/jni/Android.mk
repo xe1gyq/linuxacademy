6b3479adf3a17c0a89e834fc669c418dc9e77015
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 11:19:03 -0700

    Android: Product Implementation HAL Initialization
    Provide our implementation which initializes our HAL module
    
    Balam by Netavam Inc.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/netavam/balam/framework/libbalamator_jni/jni/Android.mk
+++ android/netavam/balam/framework/libbalamator_jni/jni/Android.mk
@@ -1,1 +1,11 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := com_netavam_android_lib_balam_LibBalam.cpp
+LOCAL_C_INCLUDES += $(JNI_H_INCLUDE) $(LOCAL_PATH)/../../../include/
+LOCAL_CFLAGS += -g -O0 -Wno-unused-parameter
+LOCAL_SHARED_LIBRARIES := libcutils libhardware libnativehelper
+LOCAL_MODULE := libbalamator_jni
+
+include $(BUILD_SHARED_LIBRARY)
