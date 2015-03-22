2906daacdbb7d7c6d812302ea525c715034b431e
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 06:29:42 -0700

    Android: NDK Balamex UI Functions
    Implement functions for our UI
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/Balamex/src/com/example/balamex/BalamexActivity.java
+++ android/Balamex/src/com/example/balamex/BalamexActivity.java
@@ -1,16 +1,35 @@
 package com.example.balamex;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class BalamexActivity extends Activity {
+public class BalamexActivity extends Activity implements OnClickListener {
 
+	private EditText input;
+	private RadioGroup type;
+	private TextView output;
+	private Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_balamex);
+		this.input = (EditText) super.findViewById(R.id.input);
+		this.type = (RadioGroup) super.findViewById(R.id.type);
+		this.output = (TextView) super.findViewById(R.id.output);
+		this.button = (Button) super.findViewById(R.id.button);
+		this.button.setOnClickListener(this);
 	}
 
 	@Override
@@ -30,5 +49,45 @@ public class BalamexActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View view) {
+		String s = this.input.getText().toString();
+		if (TextUtils.isEmpty(s)) {
+			return;
+		}
+		final ProgressDialog dialog = ProgressDialog.show(this, "", "Calculating", true);
+		final long n = Long.parseLong(s);
+		new AsyncTask<Void, Void, String>() {
+			
+			@Override
+			protected String doInBackground(Void... params) {
+
+				long result = 0;
+				long t = System.currentTimeMillis();
+				switch(BalamexActivity.this.type.getCheckedRadioButtonId()) {
+					case R.id.type_balamator_jon:
+						result = BalamexLib.balamatorJOn(n);
+						break;
+					case R.id.type_balamator_joff:
+						result = BalamexLib.balamatorJOff(n);
+						break;
+					case R.id.type_balamator_ion:
+						result = BalamexLib.balamatorIOn(n);
+						break;
+					case R.id.type_balamator_ioff:
+						result = BalamexLib.balamatorIOff(n);
+						break;
+				}
+				t = System.currentTimeMillis() - t;
+				return String.format("Balamator %d in %d", result, t);
+			}
+			@Override
+			protected void onPostExecute(String result) {
+				dialog.dismiss();
+				BalamexActivity.this.output.setText(result);
+			}
+		}.execute();
 	}
 }
