dcabdc58006677feaf81178cc3237658755c1def
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 04:06:35 -0700

    Android: NDK Balamex Android Compilation
    Include Android.mk to compile
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ android/Balamex/jni/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := com_example_balamex_BalamexLib.c
+LOCAL_MODULE := com_example_balamex_BalamexLib
+
+include $(BUILD_SHARED_LIBRARY)
