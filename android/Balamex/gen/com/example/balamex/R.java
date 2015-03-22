58d811add9a502de7cf97602a820da40156b5266
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 05:40:01 -0700

    Android: NDK Balamex UI
    Create our UI
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/Balamex/gen/com/example/balamex/R.java
+++ android/Balamex/gen/com/example/balamex/R.java
@@ -25,7 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int button=0x7f080007;
+        public static final int input=0x7f080001;
+        public static final int output=0x7f080008;
+        public static final int textView1=0x7f080000;
+        public static final int type=0x7f080002;
+        public static final int type_balamator_ioff=0x7f080005;
+        public static final int type_balamator_ion=0x7f080006;
+        public static final int type_balamator_joff=0x7f080004;
+        public static final int type_balamator_jon=0x7f080003;
     }
     public static final class layout {
         public static final int activity_balamex=0x7f030000;
@@ -34,8 +43,13 @@ public final class R {
         public static final int balamex=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050007;
         public static final int app_name=0x7f050000;
+        public static final int balamatorIOff=0x7f050005;
+        public static final int balamatorIOn=0x7f050004;
+        public static final int balamatorJOff=0x7f050003;
+        public static final int balamatorJOn=0x7f050002;
+        public static final int button=0x7f050006;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
