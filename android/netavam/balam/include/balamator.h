57d4bc20147cf2f9c2b7a5e208dd776a14889bc1
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 10:08:09 -0700

    Android: Product Library Rework
    Make our Balamator library use a temporal sysfs entry plus its
    rework of content
    
    Balam by Netavam Inc.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/netavam/balam/include/balamator.h
+++ android/netavam/balam/include/balamator.h
@@ -5,22 +5,17 @@
 #include <sys/cdefs.h>
 #include <sys/types.h>
 #include <hardware/hardware.h>
-#include <log/log.h>
-#include <log/logger.h>
 
 __BEGIN_DECLS
 
 #define BALAMATOR_HARDWARE_MODULE_ID "balamator"
 
 struct balamator_device_t {
-  /*
-   * Common HAL device structure.
-   * Always placed at the head of the module definition.
-   */
+
   struct hw_device_t common;
 
-  int (*balamator_on)(struct balamator_device* dev, unsigned int status);
-  int (*balamator_off)(struct balamator_device* dev);
+  int (*balamator_on)(struct balamator_device_t* dev, unsigned int status);
+  int (*balamator_off)(struct balamator_device_t* dev, unsigned int status);
 };
 
 __END_DECLS
--- android/netavam/balam/lib/libbalamator/libbalamator.c
+++ android/netavam/balam/lib/libbalamator/libbalamator.c
@@ -6,73 +6,36 @@
 #include <fcntl.h>
 #include <time.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 
-static const char THE_DEVICE[] = "/sys/kernel/balamator/status";
-
-static int balamator_exists() {
-    int fd;
-
-    fd = TEMP_FAILURE_RETRY(open(THE_DEVICE, O_RDWR));
-    if(fd < 0) {
-        ALOGE("balamator/status file does not exist : %d", fd);
-        return 0;
-    }
-
-    close(fd);
-    return 1;
-}
-
-static int sendit(unsigned int status)
-{
-    int to_write, written, ret, fd;
-
-    char value[20];
-
-    fd = TEMP_FAILURE_RETRY(open(THE_DEVICE, O_RDWR));
-    if(fd < 0) {
-        return -errno;
-    }
-
-    to_write = snprintf(value, sizeof(value), "%u\n", status);
-    written = TEMP_FAILURE_RETRY(write(fd, value, to_write));
-
-    if (written == -1) {
-        ret = -errno;
-    } else if (written != to_write) {
-        ret = -EAGAIN;
-    } else {
-        ret = 0;
-    }
-
-    errno = 0;
-    close(fd);
-
-    return ret;
-}
+static const char BALAMATOR_TEMP[] = "/sys/class/net/eth0/statistics/rx_bytes";
 
 static int balamator_on(struct balamator_device_t* balamdev, unsigned int status)
 {
-	return sendit(status);
+	int fd;
+	int len;
+	int ii;
+	char value[PAGE_SIZE];
+	fd = open(BALAMATOR_TEMP, O_RDONLY);
+	if (fd < 0)
+		return errno;
+	len = read(fd, value, sizeof(value));
+	ii = atoi(value);
+	close(fd);
+	return(ii);
 }
-
 
 static int balamator_off(struct balamator_device_t* balamdev, unsigned int status)
 {
-	return sendit(0);
+	return 0;
 }
 
 static int open_balamator(const struct hw_module_t *module, char const *name,
    struct hw_device_t **device) {
 
-    if (!balamator_exists()) {
-        ALOGE("balamator/status device does not exist");
-        return -ENODEV;
-    }
-
     struct balamator_device_t *dev = malloc(sizeof(struct balamator_device_t));
     if (!dev) {
-        ALOGE("Can not allocate memory for balamator/status device");
         return -ENOMEM;
     }
 
