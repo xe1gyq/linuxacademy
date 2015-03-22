6a65cf7fdba6a62298517366eda92f9c8be10aec
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 11:28:37 -0700

    Android: Product Package Compilation
    Include the recently added packages for compilation
    
    Balam by Netavam Inc.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- android/netavam/balam/packages.mk
+++ android/netavam/balam/packages.mk
@@ -1,2 +1,7 @@
 PRODUCT_PACKAGES += balamator.default
 PRODUCT_PACKAGES += balamator
+
+PRODUCT_PACKAGES += \
+        com.netavam.android.lib.balam \
+        com.netavam.android.lib.balam.xml \
+        balamator_jni
