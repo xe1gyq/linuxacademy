96bb5c570dc95f3ac8e87372f6b8edf9abf1e633
Abraham Arce <xe1gyq@gmail.com>
2015-03-22 03:37:52 -0700

    Android: NDK Balamex
    Example of working with NDK by creating Balamex
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ android/Balamex/src/com/example/balamex/BalamexLib.java
@@ -0,0 +1,14 @@
+package com.example.balamex;
+
+public class BalamexLib {
+	
+	public static long balamatorJOn(long status) {
+		return 1;
+	}
+	public native static long balamatorIOn(long status);
+	
+	public static long balamatorJOff(long status) {
+		return 0;
+	}
+	public native static long balamatorIOff(long status);
+}
