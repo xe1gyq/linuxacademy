b03c9667ef4c5388ae9dfe884ae9db8a17a79f43
Abraham Arce <xe1gyq@gmail.com>
2015-05-30 20:40:31 -0500

    Scrtips: Debian GNU Radio
    GNU Radio Installation
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- scripts/debiansetup.sh
+++ scripts/debiansetup.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
+# apt-get
+
 apt-get update
-apt-get install git git-email
+apt-get install git git-email htop python-pip python-dev python-qt4 cmake pkg-config libusb-dev libusb-1.0-0-dev gnuradio gnuradio-dev hackrf gr-osmosdr rtl-sdr gqrx-sdr
+
+# pip
+
+pip install psutil
+
+# GNU Radio
+git clone git://github.com/pybombs/pybombs
+cd pybombs
+./pybombs install gnuradio
+./pybombs install hackrf
+nano /etc/ld.so.conf.d/gnuradio.conf
+ /home/xe1gyq/Projects/Radio/lib/
+ldconfig
+
+pip install psutil
